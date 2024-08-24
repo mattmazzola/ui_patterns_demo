174918e2ef800c4d098aca74f1d5f7ee7fc2aeaa
Matt Mazzola <mattmazzola@live.com>
2024-08-24 13:27:51 -0700

    Create basic counter

--- UiPatternsDemo/ui_builder.py
+++ UiPatternsDemo/ui_builder.py
@@ -1,3 +1,4 @@
+import omni.log
 import omni.ui as ui
 from omni.isaac.ui.element_wrappers import UIWidgetWrapper
 
@@ -12,18 +13,74 @@ class UIBuilder:
             ui_elem.cleanup()
 
     def build_ui(self):
-        self._create_example_frame()
+        ui.Label(
+            "UI Patterns Demo",
+            alignment=ui.Alignment.CENTER,
+            style={"font_size": 28},
+        )
+        ui.Label(
+            "Counter",
+            alignment=ui.Alignment.CENTER,
+            style={"font_size": 20},
+        )
+        self._counter_int_model = self._create_counter()
+        self._create_value_description_frame(self._counter_int_model)
 
-    def _create_example_frame(self):
-        self._buttons_container = ui.ScrollingFrame(height=40)
+    def _create_counter(self):
+        with ui.HStack():
+            ui.Spacer()
+            with ui.HStack(spacing=0, width=200, height=50, alignment=ui.Alignment.CENTER):
+                counter_int_model = ui.SimpleIntModel(0)
+
+                def _on_click_decrement():
+                    omni.log.info(f"Decrement - Counter Value: {counter_int_model.as_int}")
+                    counter_int_model.set_value(counter_int_model.as_int - 1)
+
+                def _on_click_increment():
+                    omni.log.info(f"Increment - Counter Value: {counter_int_model.as_int}")
+                    counter_int_model.set_value(counter_int_model.as_int + 1)
+
+                ui.Button(text="Dec", clicked_fn=_on_click_decrement)
+
+                counter_label = ui.Label(
+                    str(counter_int_model.as_int),
+                    alignment=ui.Alignment.CENTER,
+                    style={"font_size": 24},
+                )
+
+                def update_value(model: ui.SimpleIntModel):
+                    counter_label.text = str(model.as_int)
+
+                counter_int_model.add_value_changed_fn(update_value)
+
+                ui.Button(text="Inc", clicked_fn=_on_click_increment)
+
+            ui.Spacer()
+
+        return counter_int_model
+
+    def _create_value_description_frame(self, int_model: ui.SimpleIntModel):
+        description_frame = ui.Frame(height=40)
 
         # Overwrite frame contents whenever the model changes
         # Create Frame first, use reference inside the callback
-        def changed(model: ui.SimpleIntModel, recreate_ui=self._buttons_container):
-            with recreate_ui:
-                with ui.HStack():
-                    for i in range(model.as_int):
-                        ui.Button(f"Button #{i}")
+        def int_changed(model: ui.SimpleIntModel):
+            with description_frame:
+                if model.as_int <= -5:
+                    ui.Label(
+                        "Low",
+                        alignment=ui.Alignment.CENTER,
+                        style={"font_size": 42},
+                    )
 
-        model = ui.IntDrag(min=0, max=10).model
-        model.add_value_changed_fn(changed)
+                elif model.as_int >= 5:
+                    ui.Label(
+                        "High",
+                        alignment=ui.Alignment.CENTER,
+                        style={"font_size": 42},
+                    )
+
+                else:
+                    ui.Spacer()
+
+        int_model.add_value_changed_fn(lambda model: int_changed(model))
